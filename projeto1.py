726300d47cca1f2e5128b9d647fdcb95321e3a9d
infernalskk <129634483+infernalsk1@users.noreply.github.com>
2026-08-12 15:19:15 -0300

    Add files via upload

---
+++ projeto1.py
@@ -0,0 +1,14 @@
+# Variaveis e tipos de dados para treino
+nome = 'Elloy'
+sobrenome = 'Neves'
+idade = 15
+ano_nascimento = 2011
+maior_de_idade = 18
+ano_atual = 2026
+
+print('NOME:', nome)
+print('SOBRENOME:', sobrenome)
+print('IDADE:', idade)
+print('ANO DE NASCIMENTO:', ano_nascimento)
+print('ANO ATUAL:', ano_atual)
+print('MAIOR DE IDADE?:', idade >= maior_de_idade)
