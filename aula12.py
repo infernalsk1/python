ba5842e8b6b8d68580bd9ab6a1cefccee68b64ca
infernalskk <129634483+infernalsk1@users.noreply.github.com>
2026-08-20 22:09:58 -0300

    Add files via upload

---
+++ aula12.py
@@ -0,0 +1,9 @@
+Nome = 'Beatriz'
+Peso = 63
+Altura = 1.52
+Imc = ...
+
+print('Nome:', Nome)
+print('O seu peso é:', Peso)
+print('A sua altura é:', Altura)
+print('O seu IMC é:', (Peso / Altura / Altura))
